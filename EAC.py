7ef110a0825f26ee032095d02c28d5cb26e7117d
albanus korir <107255667+albanuskorir@users.noreply.github.com>
2023-01-31 15:16:24 +0300

    Update EAC.py

--- EAC.py
+++ EAC.py
@@ -1,3 +1,11 @@
-list = ["Kenya", "uganda", "Tanzania"]
-print("list" in list)
+# write a program to check if you are eligible to vote: and you must be an East African Citizen (Kenya, Tanzania, Uganda, and above 18 years)
+age = int(input("Enter your age: "))
+country = input("country: ").caption()
+counries = ["Kenya", "uganda", "Tanzania"]
+if age >= 18 and "country" in "countries":
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
+    print(age)
+    print(country)
 
