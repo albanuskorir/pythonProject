f51d47c8ba46389230a5751ea604597cedb6d19f
albanus korir <107255667+albanuskorir@users.noreply.github.com>
2023-02-24 13:19:08 +0300

    Update v_sphere.py

--- v_sphere.py
+++ v_sphere.py
@@ -1,7 +1,9 @@
-# Calculate the area of a sphere
+# Prompt user to enter pie and radius of sphere and calculate the volume.
 def volume_of_sphere(pie, r):
     volume = (4 / 3 * pie * r ** 3)
     return volume
 
 
-print(volume_of_sphere(22/7, 14))
+pie = float(input("Enter pie: "))
+r = int(input("Enter the radius of a sphere: "))
+print(volume_of_sphere(pie, r))
